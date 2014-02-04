d720ddb76d496c880ef5f1ce3b908133f4d84da7
Anton Amlinger <antonamlinger@gmail.com>
2014-02-04 12:21:35 +0100

    Markup and styling improved overall

--- Laboration 1/client.js
+++ Laboration 1/client.js
@@ -1,91 +1,115 @@
 window.onload = function() {
+    // Load view when window is loaded.
 	updateView(localStorage.getItem('clientToken'));   
 };
 
-function bindWelcomEvents(){
+/*
+ * Binds all events (click events) to the welcome view, should be
+ * called when view is fully loaded, since the elements must exist
+ * to be abel to have evenets.
+ */
+function bindWelcomEvents() {
     document.getElementById('signup_button').onclick = function(event) {
         
         event.preventDefault();
 		var message	= this.parentNode.getElementsByClassName('message_area')[0];
 			
-			
+		
+        /*
+          In order to send the data to the server, some validation
+          is performed before sending:
+          
+          * No fields may be empty; all are mandatory.
+          * The password field must contain the same value as 
+            the repeat password field
+          
+          If these criteria are not met, the function is abandoned
+          by issuing a return. This is all beccause we want to limit
+          the requests sent and recieved from the server.
+         */
         if(!validateEmptyFields(this)) {
-			// Fields are empty, displaying 
 			message.innerHTML = 'All fields are mandatory.';
 			return false;
         } else if (!validatePasswordFields(this)) {
-			// Fields are empty, displaying 
 			message.innerHTML = 'Password fields must contain the same value.';
 			return false;
 		}
         
         var form = document.getElementById('signup_form');
         
+        // Fetch all parameters passed from the fomr.
         var formObject = {
-            firstname : form.elements['firstname'].value,
-            familyname : form.elements['familyname'].value,
-            gender : form.elements['gender'].value,
-            city : form.elements['city'].value,
-            country : form.elements['country'].value,
-            email : form.elements['email'].value,
-            password: form.elements['password'].value    
+            firstname       : form.elements['firstname'].value,
+            familyname      : form.elements['familyname'].value,
+            gender          : form.elements['gender'].value,
+            city            : form.elements['city'].value,
+            country         : form.elements['country'].value,
+            email           : form.elements['email'].value,
+            password        : form.elements['password'].value    
         }
         
-        var answer = serverstub.signUp(formObject);
+        var response = serverstub.signUp(formObject);  
+        message.innerHTML = response.message;
         
-        message.innerHTML = answer.message;
+        // Erase all input values if 
+        if(response.success) {
+            form.elements['firstname'].value    = "";
+            form.elements['familyname'].value   = "";
+            form.elements['city'].value         = "";
+            form.elements['country'].value      = "";
+            form.elements['email'].value        = "";
+            form.elements['password'].value     = "";
+            form.elements['re_password'].value  = "";
         
-        if(answer.success){
-            form.elements['firstname'].value = "";
-            form.elements['familyname'].value = "";
-            form.elements['gender'].value = "";
-            form.elements['city'].value = "";
-            form.elements['country'].value = "";
-            form.elements['email'].value = "";
-            form.elements['password'].value = "";
-            form.elements['re_password'].value = "";
-        }
-        
-        else{
+        } else {
+            /* Indicate that an error has occoured on this field by adding
+                a red border to the input */
             form.elements['email'].style.borderColor = "darkred";
         }
         
         return false;
     };
     
-document.getElementById('login_button').onclick = function(event) {
-			
-	// Always prevent from actually sending the request.
-		event.preventDefault();
-	 
-	// Fetch the message area for displaying error messages.
-	var message	= this.parentNode.getElementsByClassName('message_area')[0];
-	
-	/* 	Only validation for Login area is the check for all fields being
-			non-empty. If this is not OK, display error message and exit 
-			function. */
-	if(!validateEmptyFields(this)) {
-		message.innerHTML = 'Please fill out an <b>Email</b> and  a <b>Password</b>.';
-		return false;
-	}
-	
-	var elements = this.parentNode, 
-        token = serverstub.signIn(elements["email"].value, elements["password"].value);
-    
-    if(token.success){
-        localStorage.setItem('clientToken',token.data);
-        updateView(localStorage.getItem('clientToken'));
-    }
-    else{
-        elements['email'].style.borderColor = 'darkred';
-        elements['password'].style.borderColor = 'darkred';
-        message.innerHTML = token.message;
-    }
+    document.getElementById('login_button').onclick = function(event) {
+    			
+    	// Always prevent from actually sending the request.
+    		event.preventDefault();
+    	 
+    	// Fetch the message area for displaying error messages.
+    	var message	= this.parentNode.getElementsByClassName('message_area')[0];
+    	
+    	/* 	Only validation for Login area is the check for all fields being
+    		non-empty. If this is not OK, display error message and exit 
+    		function. */
+    	if(!validateEmptyFields(this)) {
+    		message.innerHTML = 'Please fill out an <b>Email</b> and  a <b>Password</b>.';
+    		return false;
+    	}
+    	
+    	var elements = this.parentNode, 
+            token = serverstub.signIn(elements["email"].value, elements["password"].value);
         
-	return false;
+        /* Present the new view if login request is successful, otherwise
+           indicate the error by a red border around the input area. */
+        if(token.success){
+            localStorage.setItem('clientToken',token.data);
+            updateView(localStorage.getItem('clientToken'));
+        
+        } else {
+            elements['email'].style.borderColor     = 'darkred';
+            elements['password'].style.borderColor  = 'darkred';
+            message.innerHTML = token.message;
+        }
+            
+    	return false;
     };
 }
 
+/**
+ * Given a context, this function returns true or false
+ * depending on the comparison between two fields with
+ * classname repeat_password. 
+ */
 function validatePasswordFields(context) {
     var pwds = context.parentNode.getElementsByClassName('repeat_password');
     
@@ -101,6 +125,12 @@ function validatePasswordFields(context) {
     return true;
 }
 
+/**
+ * Check for empty fields given in the context, e.g.
+ * fields without any value property set. If this is 
+ * not the case, false is returned and the fields 
+ * without any value are marked with a red border.
+ */
 function validateEmptyFields(context){
 
     var errorExists = false;
@@ -127,7 +157,7 @@ function printPosts(wall,messages){
     
     wall.innerHTML = '';
     
-    for (var i=0;i<messages.data.length;i++){
+    for (var i=0;i < messages.data.length;i++){
         post = "<article><b>Posted by:</b> " + messages.data[i].writer + "<br>";
         post = post + messages.data[i].content;
         wall.insertAdjacentHTML('beforeend',post);
@@ -139,7 +169,8 @@ function updateView(token) {
     view = "<strong>Something went terrably wrong on the server.</strong>",
     wrap = document.getElementById('view_wrap');
 
-	if(token === undefined || token === '') {
+    /* Is anything else then null necessary? */
+	if(token === null || token === undefined || token === '') {
 		view = document.getElementById('welcomeview').innerHTML;
         wrap.innerHTML=view;
         bindWelcomEvents();
@@ -147,6 +178,7 @@ function updateView(token) {
     
     else {
 		view = document.getElementById('profileview').innerHTML;
+        console.log(view);
         wrap.innerHTML=view; 
         
         bindDefaultEvents();
@@ -192,10 +224,11 @@ function bindDefaultEvents(){
 
 function printUserData(userData,element){
     while(element){
-        if(element.tagName === 'SPAN' && element.getAttribute('name') != undefined){
-            var property = element.getAttribute('name');
-            element.innerHTML = userData.data[property];
-        }
+        var children = element.childNodes;
+
+        if(children[3])
+            children[3].innerHTML = userData.data[children[3].getAttribute('class')];
+
         element = element.nextSibling;
     }         
 }
