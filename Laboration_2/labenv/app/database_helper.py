6da4c6190c9f965c8b1533f46607b5d8f9823e7e
Sebastian Wihlborg <sebastian.wihlborg@gmail.com>
2014-02-17 09:00:39 +0100

    Lab 2 approved

---
+++ Laboration_2/labenv/app/database_helper.py
@@ -0,0 +1,157 @@
+import sqlite3
+import random
+import hashlib
+import traceback
+from flask import Flask, g
+
+#app = Flask(__name__)
+
+class DatabaseHelper:
+	def __init__(self,app):
+		self.app = app;
+		with self.app.app_context():
+			db = self.get_db()
+			with app.open_resource('database.schema', mode='r') as f:
+				db.cursor().executescript(f.read())
+				db.commit()
+				self.close_db()
+
+	def get_db(self):
+		"""Opens a new database connection if there is none yet for the
+		current application context."""
+		if not hasattr(g, 'sqlite_db'):
+			g.sqlite_db = sqlite3.connect('database.db')
+			return g.sqlite_db
+
+	def close_db(self):
+		"""Closes the database again at the end of the request."""
+		if hasattr(g, 'sqlite_db'):
+			g.sqlite_db.close()
+
+	def prepare_password(self, password):
+		md5 = hashlib.md5()
+		md5.update(password)
+		return md5.hexdigest()
+
+	def prepare_email(self, email):
+		return email.lower()
+
+	#ESCAPE STRINGS (PARAMETERS TO "EXECUTE")
+	def execute(self, query, args=[]):
+		with self.app.app_context():
+			db = self.get_db()
+			cursor = db.cursor()
+			try:
+				cursor.execute(query,args)
+			except:
+				traceback.print_exc()
+			
+			result = cursor.fetchall()
+			if(not result):
+				result = (cursor.rowcount > 0)
+			db.commit()
+			self.close_db()
+		return result
+
+	def user_exists(self, email):
+		email = self.prepare_email(email)
+		query  = "SELECT COUNT(email) FROM user WHERE email=? LIMIT 1"
+		result = self.execute(query,[email])
+		return 1 == result[0][0]
+
+	def is_user_logged_in(self, email_or_token):
+		query  = "SELECT COUNT(user) FROM token WHERE user=? OR token=? LIMIT 1"
+		email = self.prepare_email(email_or_token)
+		result = self.execute(query,[email, email_or_token])
+		return 1 == result[0][0]
+
+	def insert_user(self, email, password, firstname, familyname, gender, city, country):
+		email = self.prepare_email(email)
+		password = self.prepare_password(password)
+		query  = "INSERT INTO user VALUES (?,?,?,?,?,?,?)"
+		self.execute(query,[email, password, firstname, familyname, gender, city, country])
+
+	def show_table(self,table):
+		query = "SELECT * FROM " + table
+		result = self.execute(query)
+		if(not result):
+			return;
+		print("---------" + table + "---------")
+		for i in range(0, len(result)):
+			print(result[i])
+		print("")
+
+	def login(self,email,password):
+		email = self.prepare_email(email)
+		password = self.prepare_password(password)
+		query = "SELECT COUNT(email) FROM user WHERE email=? AND password=? LIMIT 1"
+		result = self.execute(query,[email,password])
+		return 1 == result[0][0]
+
+	def logout(self,token):
+		query = "DELETE FROM token WHERE token=?"
+		result = self.execute(query,[token])
+
+	def generate_token(self,email):
+		email = self.prepare_email(email)
+		letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+		token = ""
+		for i in range(0, 36):
+			token += letters[random.randint(0, len(letters)-1)]
+
+		query = "INSERT INTO token VALUES(?,?)"
+		self.execute(query,[token,email])
+
+		return token
+
+	def get_user_by_token(self,token):
+		query = "SELECT user from token where token=?"
+		result = self.execute(query,[token])
+		return result[0][0]
+
+	def change_password(self,token,old_pass,new_pass):
+		old_pass = self.prepare_password(old_pass)
+		new_pass = self.prepare_password(new_pass)
+		user = self.get_user_by_token(token)
+		query = "UPDATE user SET password=? WHERE email=? AND password=?"
+		return(self.execute(query,[new_pass,user,old_pass]))
+
+	def get_user_data_by_email(self,email):
+		email = self.prepare_email(email)
+		query = "SELECT email,first_name,family_name,gender,city,country FROM user WHERE email=?"
+		result = self.execute(query,[email])
+		return{
+				"email" 		: result[0][0],
+				"firstname" 	: result[0][1],
+				"familyname" 	: result[0][2],
+				"gender" 		: result[0][3],
+				"city" 			: result[0][4],
+				"country" 		: result[0][5]
+			}
+
+	def post_message(self,token,message,email):
+		poster = self.get_user_by_token(token)
+		email = self.prepare_email(email)
+		query = "INSERT INTO message VALUES (?,?,?)"
+		self.execute(query,[message,email,poster])
+
+	def get_messages_by_email(self,email):
+		email = self.prepare_email(email)
+		query = "SELECT * FROM message WHERE user=?"
+		result = self.execute(query, [email])
+		
+		print(1)
+
+		if(not type(result) is list):
+			print(2)
+			result = []
+		print(3)
+
+		messages = []
+		for message in result:
+			messages.append({
+				"message" 		: message[0],
+				"sender"		: message[2]
+			})
+
+		return messages
---
+++ Laboration_2/labenv/app/server.py
@@ -0,0 +1,206 @@
+from database_helper import *
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+db = DatabaseHelper(app)
+
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+
+	email = request.form['email']
+	password = request.form['password']
+	first_name = request.form['first_name']
+	family_name = request.form['family_name']
+	gender = request.form['gender']
+	city = request.form['city']
+	country = request.form['country']
+
+	if(db.user_exists(email)):
+		return jsonify(
+			success = False,
+			message = "User already exists")
+
+	db.insert_user(email, password, first_name, family_name, gender, city, country)
+
+	return jsonify(
+		success = True,
+		message = "User signed up")
+
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+
+	email = request.form['email']
+	password = request.form['password']
+
+
+
+	if(not db.user_exists(email)):
+		return jsonify(
+			success = False, 
+			message = "User does not exists")
+
+	if(db.is_user_logged_in(email)):
+		return jsonify(
+			success = False, 
+			message = "User already logged in")
+	
+	success = db.login(email,password)
+
+	if(success):
+		token = db.generate_token(email)
+		return jsonify(
+			success = True, 
+			message = "User logged in",
+			data = token)
+
+	return jsonify(
+		success = False, 
+		message = "Wrong password")
+
+@app.route('/sign_out', methods=['POST'])
+def sign_out():
+
+	token = request.form['token']
+	
+	#This check is not acctually needed, but helps to send a good response
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+	
+	db.logout(token)
+
+	return jsonify(
+		success = True, 
+		message = "User successfully logged out")
+
+@app.route('/change_password', methods=['POST'])
+def change_password():
+
+	token = request.form['token']
+
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+
+	old_pass = request.form['old_pass']
+	new_pass = request.form['new_pass']
+
+	if(db.change_password(token,old_pass,new_pass)):
+		return jsonify(
+			success = True, 
+			message = "Password changed")
+
+	return jsonify(
+			success = False, 
+			message = "Wrong password")
+
+@app.route('/get_user_data_by_email', methods=['POST'])
+def get_user_data_by_email():
+
+	token = request.form['token']
+
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+
+	email = request.form['email']
+	result = db.get_user_data_by_email(email)
+
+	return jsonify(
+		success = True,
+		message = "User data recived", 
+		data = result)
+
+@app.route('/get_user_data_by_token', methods=['POST'])
+def get_user_data_by_token():
+
+	token = request.form['token']
+	
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+
+	email = db.get_user_by_token(token)
+
+	result = db.get_user_data_by_email(email)
+
+	return jsonify(
+		success = True,
+		message = "User data recived",
+		data = result)
+
+@app.route('/post_message', methods=['POST'])
+def post_message():
+
+	token = request.form['token']
+	message = request.form['message']
+	email = request.form['email']
+
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+
+	if(not db.user_exists(email)):
+		return jsonify(
+			success = False,
+			message = "Recipient does not exists")
+
+	db.post_message(token,message,email)
+
+	return jsonify(
+			success = True,
+			message = "Message successfully posted")
+
+@app.route('/get_messages_by_email', methods=['POST'])
+def get_messages_by_email():
+
+	token = request.form['token']
+	email = request.form['email']
+
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+
+	if(not db.user_exists(email)):
+		return jsonify(
+			success = False,
+			message = "User does not exists")
+
+	result = db.get_messages_by_email(email)
+
+	return jsonify(
+		success = True,
+		message = "User messages recived",
+		data = result)
+
+@app.route('/get_messages_by_token', methods=['POST'])
+def get_messages_by_token():
+
+	token = request.form['token']
+
+	if(not db.is_user_logged_in(token)):
+		return jsonify(
+			success = False, 
+			message = "User not logged in")
+
+	email = db.get_user_by_token(token)
+
+	result = db.get_messages_by_email(email)
+
+	return jsonify(
+		success = True,
+		message = "User messages recived",
+		data = result)
+
+if __name__ == '__main__':
+	db.show_table("user")
+	db.show_table("token")
+	db.show_table("message")
+	app.run()
